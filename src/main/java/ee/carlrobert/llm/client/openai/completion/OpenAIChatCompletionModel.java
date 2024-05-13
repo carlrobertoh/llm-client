becc0223bd07bda1b493b13e2e9aa423accafc3a
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-05-13 23:43:44 +0300

    feat: add gpt-4o model

--- src/main/java/ee/carlrobert/llm/client/openai/completion/OpenAIChatCompletionModel.java
+++ src/main/java/ee/carlrobert/llm/client/openai/completion/OpenAIChatCompletionModel.java
@@ -11,7 +11,8 @@ public enum OpenAIChatCompletionModel implements CompletionModel {
   GPT_4_32k("gpt-4-32k", "GPT-4 (32k)", 32768),
   GPT_4_1106_128k("gpt-4-1106-preview", "GPT-4 Turbo (Legacy) (128k)", 128000),
   GPT_4_0125_128k("gpt-4-0125-preview", "GPT-4 Turbo (128k)", 128000),
-  GPT_4_VISION_PREVIEW("gpt-4-vision-preview", "GPT-4 Vision Preview (128k)", 128000),;
+  GPT_4_VISION_PREVIEW("gpt-4-vision-preview", "GPT-4 Vision Preview (128k)", 128000),
+  GPT_4_O("gpt-4o", "GPT-4o (128k)", 128000);
 
   private final String code;
   private final String description;
